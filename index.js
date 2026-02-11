eed33fb13a28022ec3738f5fb83737ba39a50662
PANFILI <mistermika100205@gmail.com>
2026-02-11 11:35:57 +0100

    connection a mangoDB

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const connectDB = require('./db');
 
-app.listen(port || process.env.PORT, () => {
-  console.log(`Server is running on port ${port}`);
+connectDB();
+
+app.use(express.static(path.join(__dirname, 'front')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'front', 'index.html'));
 });
 
-
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
